55f3862a7fe05e31684982b1e2efb6f3a318b1fd
Binbin6 <binxue6@gmail.com>
2019-07-21 19:44:12 -0400

    FrontEnd can connect to Sequencer

---
+++ Bin/PortInformation/FEPort.java
@@ -0,0 +1,6 @@
+package PortInformation;
+
+public enum FEPort {
+    FE_PORT;
+    public final int FEPort = 8001;
+}
---
+++ Bin/PortInformation/SequencerPort.java
@@ -0,0 +1,7 @@
+package PortInformation;
+
+public enum SequencerPort {
+    SEQUENCER_PORT;
+    public final int sequencerPort = 8002;
+
+}
